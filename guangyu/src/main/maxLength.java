b6bdc8c532d7f0006cc821c42ef39d5944e83130
lyc <920442875@qq.com>
2017-05-04 20:47:55 +0800

    3

--- guangyu/src/main/maxLength.java
+++ guangyu/src/main/maxLength.java
@@ -30,7 +30,7 @@ public class maxLength {
     }  
     public static void main(String[] args) {  
         // TODO Auto-generated method stub  
-        String str="";  
+        String str="aaaaaaaa";  
         System.out.println(lengthOfLongestSubstring(str));  
     }  
   
